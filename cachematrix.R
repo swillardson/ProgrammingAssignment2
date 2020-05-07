f12b42ce313bfa2ba4f2529b372d97569bf5de28
swillardson <spencer.l.willardson@gmail.com>
2020-05-07 12:18:06 +0600

    Assingment 2 completion upload 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This exercise uses two functions.  The first creates a matrix list with the solved matrix function. The second function determines whether or not the cached matrix is the same as one being tested, and if it is, then it passes the already solved matrix as the solution.
 
-## Write a short comment describing this function
+## This function solves the matrix and caches its inverse function using the 
+## solve() function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- m
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function returns the solution to the solved matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmatrix(m)
+  m        
 }
